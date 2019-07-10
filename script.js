7d78144fa0ddc516092b385a8e41985a64e78023
areebakhan123 <49521463+areebakhan123@users.noreply.github.com>
2019-07-10 20:41:24 +0500

    Arrays

---
+++ script.js
@@ -0,0 +1,72 @@
+let student1 = {
+    name:"areeba khan",
+   age : 21,
+   profession:"teacher/web developer",
+    phone: "021-1234567"
+}
+
+let student2 = {
+    name:"farah khan",
+    age: "19",
+    phone: "021-1234567"
+   
+}
+
+let student3 = {
+    name:"sarah khan",
+    age: "31",
+    phone: "021-1234567",
+    profession:"software engineer"
+   
+}
+
+let student4 = {
+    name:"javeriya khan",
+    age: "16",
+    phone: "021-1234567",
+   
+}
+
+let student5 = {
+    name:"rida khan",
+    age: "45",
+    phone: "021-1234567",
+    profession:"tailor"
+   } 
+
+let student6 = {
+    name:"masooma khan",
+    age: "52",
+    phone: "021-1234567"
+   
+}
+let student7 = {
+    name:"zanjil khan",
+    age: "42",
+    phone: "021-1234567",
+   
+}
+let student8 = {
+    name:"reham khan",
+    age: "62",
+    phone: "021-1234567"
+   
+}
+let student9 = {
+    name:"nida khan",
+    age: "88",
+    phone: "021-1234567"
+   
+}
+let student10 = {
+    name:"warda khan",
+    age: "21",
+    phone: "021-1234567",
+    profession:"banker"
+   
+}
+
+console.log(student1,student2,student3,student4,student5,student6,student7,student8,student9,student10);
+
+ let students = [1,2,3,4,5,6,7,8,9,10];
+ console.log(students);
